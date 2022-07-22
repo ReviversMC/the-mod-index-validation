7bb744ca2a1e4b6fadd8eadd83bb43e1f7f868c5
Cloudate9 <65006925+Cloudate9@users.noreply.github.com>
2022-07-22 12:17:48 -0700

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 plugins {
     id ("com.github.johnrengelman.shadow") version "7.1.2"
-    kotlin("jvm") version "1.7.0"
+    kotlin("jvm") version "1.7.10"
 }
 
 group = "com.github.reviversmc.themodindex.validation"
-version = "4.3.0"
+version = "4.3.1"
 
 repositories {
     mavenCentral()
@@ -12,7 +12,7 @@ repositories {
 }
 
 dependencies {
-    api("com.github.reviversmc:the-mod-index-api:7.2.1")
+    api("com.github.reviversmc:the-mod-index-api:8.0.0")
     api("org.jetbrains.kotlinx:kotlinx-coroutines-core-jvm:1.6.3")
 }
 
